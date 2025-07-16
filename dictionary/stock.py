94898088498176b57dae7b8ff950d3ef44bb755a
Mario Zahariev <webbersof@gmail.com>
2025-07-16 18:16:31 +0300

    Solutions to tasks from Dictionary lecture

---
+++ dictionary/stock.py
@@ -0,0 +1,16 @@
+stock_data = input().split()
+
+stock = {}
+
+for i in range(0, len(stock_data), 2):
+    product = stock_data[i]
+    quantity = int(stock_data[i + 1])
+    stock[product] = quantity
+
+products_to_search = input().split()
+
+for product in products_to_search:
+    if product in stock:
+        print(f"We have {stock[product]} of {product} left")
+    else:
+        print(f"Sorry, we don't have {product}")
